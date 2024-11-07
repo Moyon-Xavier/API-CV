a427b33c4517eca889553f80fe4c4129c02a9e98
moyonxavier@gmail.com <moyonxavier@gmail.com>
2024-11-07 18:31:27 +0100

    modif Docker Compose

---
+++ docker/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get update -y && apt-get install -y maven
+
+#mvn clean package && cp -r /usr/local/tomcat/webapp/target/test /usr/local/tomcat/webapp
---
+++ project/src/main/java/fr/xavier/moyon/DAO/Project/PeriodDAO.java
@@ -0,0 +1,68 @@
+package fr.xavier.moyon.DAO.Project;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.xavier.moyon.DTO.Project.PeriodDTO;
+import fr.xavier.moyon.utils.DB;
+import fr.xavier.moyon.utils.Exception.DataNotFoundException;
+
+public class PeriodDAO {
+    public List<PeriodDTO> getAllPeriod() {
+        List<PeriodDTO> periodList = new ArrayList<>();
+        Connection con = null;
+        try {
+            con = DB.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * From Period");
+            while (rs.next()) {
+                int perno = rs.getInt(1);
+                String title = rs.getString(2);
+                periodList.add(new PeriodDTO(perno, title));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return periodList;
+    }
+
+    public PeriodDTO getPeriodById(int id) {
+        PeriodDTO period = null;
+        Connection con = null;
+        try {
+            con = DB.getConnection();
+            PreparedStatement ps = con.prepareStatement("select * From Period Where perno = ?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int perno = rs.getInt(1);
+                String title = rs.getString(2);
+                period = new PeriodDTO(perno, title);
+            } else {
+                period = new PeriodDTO(-1, "periode non trouvée dans la base de données");
+                throw new DataNotFoundException("periode " + id + " non trouvée");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return period;
+    }
+}
